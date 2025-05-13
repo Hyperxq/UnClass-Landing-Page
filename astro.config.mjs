a1efba36f8efa03aae574e78e8bffa81088060aa
Daniel Ramírez Barrientos <barrientosd9@gmail.com>
2025-05-12 18:09:30 -0600

    Add Text component and integrate it into Home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,20 @@ import react from '@astrojs/react';
 
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
-export default defineConfig(({ mode }) =>({
+export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss()
+    ],
     resolve: {
       // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
       // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-      alias: mode === 'production'
+      alias: import.meta.env.MODE === 'production'
       ? { 'react-dom/server': 'react-dom/server.edge' }
       : {},
     },
   },
 
   integrations: [sitemap(), react()],
-  adapter: cloudflare({
-      imageService: 'cloudflare'
-  }),
-}));
+  adapter: cloudflare(),
+});
---
+++ src/components/react/Text.tsx
@@ -0,0 +1,3 @@
+export const Text = () => {
+  return <h1 className="text-3xl font-bold underline">Welcome to Astro!</h1>
+}
